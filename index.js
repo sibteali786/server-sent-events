bdde68c60991b53121d12e8f36e96acf878e5563
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-01-01 11:27:49 +0500

    incremental fucntion

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const app = require("express")();
 app.get("/", (req, res) => res.send("hello"));
 app.get("/stream", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
-  res.write("data: hello\n\n");
+  send(res);
 });
 app.listen(8080, () => console.log("Server is running on port 3000"));
+let i = 0;
+function send(res) {
+  res.write(`data: hello ${i}\n\n`);
+  setTimeout(() => {
+    i++;
+    send(res);
+  }, 1000);
+}
